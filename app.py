474ebe238fb06b7c1d7d2247d96b0a4b25c5936a
swupel <101061339+swupel@users.noreply.github.com>
2024-07-08 12:54:10 +0200

    Finalised multi chain UI

--- app.py
+++ app.py
@@ -24,13 +24,13 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    address = db.Column(db.String(100), unique=True, nullable=False)
     primains = relationship('Primain', backref='owner', lazy=True)
 
 class Primain(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     primain_name = db.Column(db.String(100), unique=True, nullable=False)
     address = db.Column(db.String(100), nullable=False)
+    chain =  db.Column(db.String(100), nullable=False)
     proof = db.Column(db.String(100), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
@@ -57,13 +57,8 @@ def signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        address = request.form['address']
         hashed_password = generate_password_hash(password, method='scrypt')
-        new_user = User(username=username, password=hashed_password, address=address)
-
-        if not crypto_methods.is_valid_eth_address(address):
-            flash('Address is invalid!', 'danger')
-            return render_template('signup.html')
+        new_user = User(username=username, password=hashed_password)
         
         try:
             db.session.add(new_user)
@@ -99,6 +94,7 @@ def login():
     return render_template('login.html')
 
 @app.route('/logout')
+@login_required
 def logout():
     session.pop('user_id', None)
     flash('You have been logged out.', 'info')
@@ -111,16 +107,14 @@ def register_primain():
         primain_name = request.form['primain_name']
         proof = request.form['proof']
         address = request.form['address']
-        
+        chain = request.form['chain_string']
+                        
         if len(current_user.primains) >= 3:
             flash('You can only own a maximum of 3 primains.', 'danger')
             return redirect(url_for('register_primain'))
 
-        new_primain = Primain(primain_name=primain_name, address=address, proof=proof, user_id=current_user.id)
-        message = f"{primain_name}{address}"
-
-        if not address == current_user.address:
-            flash('You can only assign primains to an owned address!', 'danger')
+        new_primain = Primain(primain_name=primain_name, address=address, chain=chain,proof=proof, user_id=current_user.id)
+        message = f"{primain_name}{chain}{address}"
 
         try:
             valid = crypto_methods.verify_signature(proof, message, address)
@@ -146,10 +140,10 @@ def display_address(primain_name):
     
     if primain:
         # Render the template with the address and the primain name
-        return render_template('display_address.html', address=primain.address, primain_name=primain_name, error=None)
+        return render_template('display_address.html', address=primain.address, primain_name=primain_name,network=primain.chain, error=None)
     else:
         # Render the template with an error message
-        return render_template('display_address.html', address=None, primain_name=primain_name, error='No Addresses linked to this Primain')
+        return render_template('display_address.html', address=None, primain_name=primain_name, network=None, error='No Addresses linked to this Primain')
 
 @app.route('/register_primain2', methods=['GET', 'POST'])
 @login_required
@@ -185,7 +179,7 @@ def get_address():
 
         if primain:
             # Return the address associated with the primain
-            return jsonify({'address': primain.address})
+            return jsonify({'address': primain.address, 'network':primain.chain})
 
         else:
             # If primain with the given name is not found, return an error message
